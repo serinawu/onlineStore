c54a7eff66298552e9ffd609cf8ac6c3a3ea4e16
wawatai <62425140+serinawu@users.noreply.github.com>
2023-03-06 18:31:34 +0800

    基本建置+header

---
+++ js/js.js
@@ -0,0 +1,42 @@
+//header
+$(function(){
+    $('header .hamBtn').click(function(){
+        $('header .hamNav')
+        .toggleClass('display');
+    })
+
+    $('header .hamNav i').click(function(){
+        $(this)
+        .closest('.hamNav')
+        .toggleClass('display');
+    })
+})
+
+//mainArea
+$(function(){
+    //Swiper
+    var swiper = new Swiper('.swiper', {
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        slidesPerView: 1,
+        breakpoints: {
+            1000: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        }
+    })
+})
